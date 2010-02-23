ca13121b856dda1af80047ccd41097ac914bc868
Ionut G. Stan <ionut.g.stan@gmail.com>
2010-02-23 16:53:42 +0200

    Give more meaningful names to variables

--- src/ro/igstan/fuzzy/FuzzyDateRomanianMessages.java
+++ src/ro/igstan/fuzzy/FuzzyDateRomanianMessages.java
@@ -10,16 +10,16 @@ public class FuzzyDateRomanianMessages implements FuzzyDateMessages {
         return "acum 1 secundă";
     }
 
-    public String someSecondsAgo(long diff) {
-        return "acum " + diff + " secunde";
+    public String someSecondsAgo(long numberOfSeconds) {
+        return "acum " + numberOfSeconds + " secunde";
     }
 
     public String oneMinuteAgo() {
         return "acum 1 minut";
     }
 
-    public String someMinutesAgo(long diff) {
-        return "acum " + diff + " minute";
+    public String someMinutesAgo(long numberOfMinutes) {
+        return "acum " + numberOfMinutes + " minute";
     }
 
     public String oneHourAgo() {
